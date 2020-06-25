33458f70bbc9e07c3a67be430e4e035a3e6cee1d
Dominykas Jurkus <dominykas.jurkus1@gmail.com>
2020-06-25 10:08:14 +0300

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import time
+import random
 
 odds = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59}
 
@@ -10,5 +11,6 @@ for num in range(5):
         print("This minute seems a bit odd")
     else:
         print("Not an odd minute")
-        time.sleep(5)
+        
+    time.sleep(random.randint(1, 5))
     
