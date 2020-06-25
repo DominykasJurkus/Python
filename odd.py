bdd7cee3face710eac06eea3a2bd5db3dc465b5e
Dominykas Jurkus <dominykas.jurkus1@gmail.com>
2020-06-25 09:59:27 +0300

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,10 +1,14 @@
 from datetime import datetime
+import time
 
 odds = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59}
 
 rgiht_this_minute = datetime.today().minute
 
-if rgiht_this_minute in odds:
-    print("This minute seems a bit odd")
-else:
-    print("Not an odd minute")
+for num in range(5):
+    if rgiht_this_minute in odds:
+        print("This minute seems a bit odd")
+    else:
+        print("Not an odd minute")
+        time.sleep(5)
+    
