1a749949a4c23975d93837159241a7b8ad7a4c30
Dominykas Jurkus <dominykas.jurkus1@gmail.com>
2020-06-29 11:41:11 +0300

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,13 +1,18 @@
 vowels = ['a', 'i', 'o', 'u', 'e']
-word = "Mississippi is a river"
+word = input("Provide a word/sentence: ")
 
-found = []
+found = {}
+
+found['a'] = 0
+found['i'] = 0
+found['o'] = 0
+found['u'] = 0
+found['e'] = 0
 
 for letter in word:
     if letter in vowels:
-        if letter not in found:
-            found.append(letter)
-        
-for vowel in found:
-    print(vowel)
+        found[letter] += 1
+
+for k, v in sorted(found.items()):
+    print(k, "was found", v, "time(s).")
 
