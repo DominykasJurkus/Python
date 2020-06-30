a24e5ab315b91cc47ba60432c6c71cda5939e93d
Dominykas Jurkus <dominykas.jurkus1@gmail.com>
2020-06-30 15:32:09 +0300

    Update vssearch.py

--- vssearch.py
+++ vssearch.py
@@ -6,7 +6,8 @@ def search4vowels():
     for vowel in found:
         print(vowel)
 
-def search4letters(phrase:str, letters:str) -> set:
+def search4letters(phrase:str, letters:str='aeiou') -> set:
     return set(letters).intersection(set(phrase))
 
 search4vowels()
+print(search4letters("Hmm what to say"))
