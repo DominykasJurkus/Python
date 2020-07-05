31bdf2c4f31e7d9c31994849a939abd7f3fc0124
Dominykas Jurkus <dominykas.jurkus1@gmail.com>
2020-07-05 22:33:53 +0300

    Update hello_flask.py

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -6,4 +6,11 @@ app = Flask(__name__)
 def hello() -> str:
     return 'Hello world from Flask!' 
 
+@app.route('/search4')
+def do_search() -> str:
+    return str(search4letters('life, the universe, and everything', 'eiru,!'))
+
+def search4letters(phrase:str, letters:str='aeiou') -> set:
+    return set(letters).intersection(set(phrase))
+
 app.run()
