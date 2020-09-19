3e32ab82b223ee18e505307efd6377f0fa5b34bd
Dominykas Jurkus <dominykas.jurkus1@gmail.com>
2020-09-19 08:54:08 +0300

    Updated Logging

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -28,10 +28,18 @@ def search4letters(phrase:str, letters:str='aeiou') -> set:
     return set(letters).intersection(set(phrase))
 
 @app.route('/viewlog')
-def view_the_log() -> str:
+def view_the_log() -> 'html':
+    contents = []
     with open('vsearch.log') as log:
-        contents = log.read()
-    return escape(contents)
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                            the_title='View Log',
+                            the_row_titles=titles,
+                            the_data=contents,)
 
 if __name__ == '__main__': 
     app.run(debug=True)
